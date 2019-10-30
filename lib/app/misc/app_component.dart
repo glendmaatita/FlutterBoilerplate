d34e12d6d27a3a823666946a889e9217f11898cf
Dhanial Rizky Wira Putra <activatedfor@gmail.com>
2019-10-30 13:52:33 +0700

    fix: bad state after closing

--- lib/app/misc/app_component.dart
+++ lib/app/misc/app_component.dart
@@ -11,7 +11,6 @@ import 'package:my_flutter/domain/entities/user.dart';
 import 'package:my_flutter/usecases/user/get_users.dart';
 
 class AppComponent {
-
   static void init() {
     Injector injector = getInjector();
     injector.registerDependency<UserApiMapper>((_) => UserApiMapper());
@@ -21,30 +20,27 @@ class AppComponent {
       (dio.transformer as DefaultTransformer).jsonDecodeCallback = parseJson;
       return dio;
     });
-    injector.registerDependency<ApiService>( (Injector injector) {
+    injector.registerDependency<ApiService>((Injector injector) {
       return ApiService(injector.getDependency<Dio>());
     });
 
-    injector.registerSingleton<UserApiRepository>( (Injector injector) {
-      return UserApiRepository(
-        injector.getDependency<ApiService>(),
-        injector.getDependency<UserApiMapper>()
-      );
+    injector.registerSingleton<UserApiRepository>((Injector injector) {
+      return UserApiRepository(injector.getDependency<ApiService>(),
+          injector.getDependency<UserApiMapper>());
     });
 
-    injector.registerSingleton<StreamController<List<User>>>( (_) {
+    injector.registerSingleton<StreamController<List<User>>>((_) {
       StreamController<List<User>> controller = StreamController();
       return controller;
     });
-    
-    injector.registerSingleton<GetUsers>( (Injector injector) {
+
+    injector.registerSingleton<GetUsers>((Injector injector) {
       return GetUsers(
         injector.getDependency<UserApiRepository>(),
-        injector.getDependency<StreamController<List<User>>>()
       );
     });
 
-    injector.registerSingleton<UsersPresenter>( (Injector injector) {
+    injector.registerSingleton<UsersPresenter>((Injector injector) {
       return UsersPresenter(injector.getDependency<GetUsers>());
     });
   }
@@ -56,7 +52,7 @@ class AppComponent {
   static parseAndDecode(String response) {
     return jsonDecode(response);
   }
-  
+
   static parseJson(String text) {
     return compute(parseAndDecode, text);
   }
